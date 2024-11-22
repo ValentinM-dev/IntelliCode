cab8aac8dc080f0e9c2ed937d5edaf7e3b415b2b
ValentinM-Dev <valentin.mamalet@hotmail.fr>
2024-11-22 11:35:10 +0100

    Update (); Update de Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import Footer from "./components/Header.js"
+import Footer from "./components/Footer.js"
+import Header from "./components/Header.js"
 
 function App() {
   return (
+    <Header />
 <Footer />
   );
 }
