6437d76c5f759046ff1e9d217d7b3c150f035d71
kevin2759 <soulhol.kevin27@gmail.com>
2024-11-24 02:16:56 +0100

    ajout le css acceuil

--- src/components/Acceuil.js
+++ src/components/Acceuil.js
@@ -4,21 +4,27 @@ import intellicook from '../assets/intellicook.png';
 import presentation from '../assets/Presentation.png';
 
 function Acceuil() {
-return (
-    <>
-<div className="logo1">
-    <img src={presentation} className="pres" />
-    <img src={intellicook} className="logoName" />
-</div>
+    return (
+        <>
+            <div className="logo1">
+                <img src={presentation} className="pres" />
+                <div>
+                    <img src={intellicook} className="logoName" />
+                    <h1 className="title">Des recettes faciles<br/> pour cuisiner Simplement</h1>
+                    <hr className="separator" />
+                    <p className="slogan">Notre plateforme vous permettra d'avoir un choix illimité<br/> de recettes en fonction des ingrédients de votre choix</p>
+                    <div className='cuisinons'>
+                        <button type="button" className='cuisine'>Cuisinons !</button>
+                    </div>
+                </div>
 
-<h1 className="title">Des recettes faciles pour cuisiner Simplement</h1>
-<hr className="separator" />
-<p className="slogan">Notre plateforme vous permettra d'avoir un choix illimité de recettes en fonction des ingrédients de votre choix</p>
+            </div>
 
-<div className='cuisinons'>
-    <button type="button" className='cuisine'>Cuisinons !</button>
-</div>
-</>
-)
+
+
+
+
+        </>
+    )
 }
 export default Acceuil;
