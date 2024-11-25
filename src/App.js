c33416e8363f047d082478a94b81fb2fe0e0418f
“Memphis-Blues” <memphisperrot@gmail.com>
2024-11-25 22:44:07 +0100

    Amélioration lisibilité code PageRecet.js et correctifs, mais seul la couleur d'arrière-plan du site s'affiche.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Footer from './components/Footer.js'
 import Header from './components/Header.js'
 import Acceuil from './components/Acceuil.js'
+import PageRecettes from './components/pageRecettes/PageRecettes.js';
 
 function App() {
   return (
     <div className="App">
     <Header />
     <Acceuil />
+    <PageRecettes/>
     <Footer />
     </div>
   );
---
+++ src/components/pageRecettes/PageRecettes.js
@@ -0,0 +1,64 @@
+import React from "react"; 
+import recettesList from "./recettesList.json";
+import "./PageRecettes.css"; /* Pour l'import du CSS */
+
+const Card = ({ recette, imgRecet }) => {
+    return (
+        <div className="recipe-card">
+            {/* Utilisation de l'image passée par les props */}
+            <img src={imgRecet} alt={recette} className="recipe-image"/>
+            <h2>{recette}</h2>
+            <button type="Button" className="cook-button">
+                Cliquez-moi pour lire en détails toute la recette !
+            </button>
+        </div>
+    );
+};
+
+const PageRecettes = () => {
+    return (
+        <div className="recipes-container">
+            {recettesList.map((recetteItem, index) => (
+                <Card key={index} recette={recetteItem.recette} imgRecet={recetteItem.imgRecet} />
+            ))}
+        </div>
+    );
+};
+
+export default PageRecettes;
+
+/* Link : OnClickEvent
+https://upmostly.com/tutorials/react-onclick-event-handling-with-examples 
+https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page 
+https://code-boxx.com/redirect-on-click-javascript/ 
+https://developer.mozilla.org/fr/docs/Learn/Getting_started_with_the_web/JavaScript_basics 
+https://fr.javascript.info/dom-nodes 
+
+On appelle l'img dans img ou assets, + le nom dans le json. 
+Si on clique sur la flèche dans la carte, ça nous ramène sur la page de la recette.
+Faire des boutons on-click pour chaque recette. 
+Pour choix selon allergènes : un filtre, mais Valentin va le faire. 
+
+Liste des recettes
+
+    Ratatouille
+    Soupe de légumes
+    Poulet curry coco
+    Quiche lorraine
+    Pâtes à la carbonara
+    Risotto aux champignons
+    Chili sin carne/con carne
+    Salade de quinoa aux légumes
+    Galettes de pommes de terre
+    Gâteau au yaourt
+    Crêpes
+    Brownies
+    Salade César au poulet
+    Omelette aux légumes
+    Lasagnes à la bolognaise
+    Poulet rôti avec légumes
+    Riz cantonais
+    Couscous aux légumes
+    Tarte aux pommes
+    Cookies au chocolat
+    */
